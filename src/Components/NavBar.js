cfde19e6e2a0b4fa35a45fb909354f19bf5a9592
Jason Liu <jasonliu2468@hotmail.com>
2021-12-27 10:42:36 -0500

    Added non-functional nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,16 @@
 const NavBar = () => {
     return (
         <div>
-            
+            <nav className='navBar'>
+                <h1>
+                    Jason Liu
+                </h1>
+                <div className='links'>
+                    <a>Link 1</a>
+                    <a>Link 2</a>
+                    <a>Link 3</a>
+                </div>
+            </nav>
         </div>
     );
 }
