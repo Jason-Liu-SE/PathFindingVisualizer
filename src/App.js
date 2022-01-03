d4b3f91846c8fa03866eb3d24f1655244cac468b
Jason Liu <jasonliu2468@hotmail.com>
2022-01-02 23:11:20 -0500

    Updated website on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar.js';
 import Visualizer from './Pages/Visualizer.js';
 
 function App() {
     return (
         <div className='App'>
-            <Router>
-                <div className='fixedContent'>
-                    <NavBar />
-                </div>
-                <div className='content'>
-                    <Routes>
-                        <Route path='/' element={<Visualizer />} />
-                        {/* <Route path='/visualizer' element={<Visualizer />} /> */}
-                        {/* <Route path='/2' element={<p>You are on page 2</p>} />
+            <div className='fixedContent'>
+                <NavBar />
+            </div>
+            <div className='content'>
+                <Routes>
+                    <Route exact path='/' element={<Visualizer />} />
+                    {/* <Route path='/visualizer' element={<Visualizer />} /> */}
+                    {/* <Route path='/2' element={<p>You are on page 2</p>} />
                         <Route path='/3' element={<p>You are on page 3</p>} /> */}
-                    </Routes>
-                </div>
-            </Router>
+                </Routes>
+            </div>
         </div>
     );
 }
